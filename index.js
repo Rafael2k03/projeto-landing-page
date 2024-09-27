b17f3a41d51adc8a7576cf295502f9c69d8ada8f
Rafael Gonçalves Miguel <goncalvesrafael047@gmail.com>
2024-09-27 08:25:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// Obtendo referências aos elementos
-const setaEsquerda = document.getElementById("Seta-esquerda");
-const setaDireita = document.getElementById("Seta-direita");
-const leonardo = document.getElementById("Leonardo");
-const samantha = document.getElementById("Samantha");
-const bruna = document.getElementById("Bruna");
+// Getting references to elements
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+const leonardo = document.getElementById("leonardo");
+const samantha = document.getElementById("samantha");
+const bruna = document.getElementById("bruna");
 
-// Função para rolar para a direita
-function rolarParaDireita() {
+// Function to scroll to the right
+function scrollRight() {
     leonardo.style.display = "none";
     bruna.style.display = "flex";
-    setaEsquerda.style.display = "flex";
-    setaEsquerda.style.marginTop = "30%";
-    setaDireita.style.display = "none";
+    leftArrow.style.display = "flex";
+    leftArrow.style.marginTop = "30%";
+    rightArrow.style.display = "none";
 }
 
-// Função para rolar para a esquerda
-function rolarParaEsquerda() {
+// Function to scroll to the left
+function scrollLeft() {
     leonardo.style.display = "flex";
     bruna.style.display = "none";
-    setaDireita.style.display = "flex";
-    setaEsquerda.style.display = "none";
+    rightArrow.style.display = "flex";
+    leftArrow.style.display = "none";
 }
